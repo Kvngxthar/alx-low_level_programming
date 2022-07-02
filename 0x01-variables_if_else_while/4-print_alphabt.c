5c3ea4bbf9edd020cbadf68bda1913ee5c62f329
Kvngxthar <esthermmakwe@gmail.com>
2022-07-02 12:17:22 -0700

    more alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of code running
+ * prints alphabet in lowercase
+ * except q and e
+ * Return: 0 is successful
+ */
+int main(void)
+{
+	char a2z;
+
+	for (a2z = 'a'; a2z <= 'z'; a2z++
+	{)
+		if (a2z != 'e' && a2z != 'q')
+			putchar(a2z);
+	}
+	putchar('\n');
+	return (0);
+}
