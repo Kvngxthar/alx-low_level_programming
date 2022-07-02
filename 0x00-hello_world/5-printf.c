f90c13bfb87945543d7e0f179ce9b9efc1e1e449
Kvngxthar <esthermmakwe@gmail.com>
2022-07-02 06:07:02 -0700

    proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * to start our program
+ * Prints a string to output
+ * Return: 0 is success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
