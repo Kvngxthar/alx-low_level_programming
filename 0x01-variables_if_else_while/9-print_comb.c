e36aa427798e24d764f84f06be2d973a40549f8b
Kvngxthar <esthermmakwe@gmail.com>
2022-07-02 13:10:58 -0700

    print

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of code running
+ * prints all single digit number of base 10
+ * using putchar to print
+ * put comma on every number
+ * Return: 0 is successful
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
