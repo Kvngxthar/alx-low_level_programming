8fd811da2a5a860744e8b76841aef32341908b77
Kvngxthar <esthermmakwe@gmail.com>
2022-07-02 14:02:00 -0700

    putcha

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of execution
+ * Prints all possible different combinations
+ * of three digits with sime conditions
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i, j, k;
+	for (i = 48 ; i < 58 ; i++)
+		for (j = i; j < 58; j++)
+			for (k = j; k < 58; k++)
+			{
+				if (i == j || j == k || i == k)
+					continue;
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	putchar('\n');
+	return (0);
+}
