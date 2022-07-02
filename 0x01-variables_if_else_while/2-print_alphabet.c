eec665f016a5c08b81aef348fdfacfacf25c5ae0
Kvngxthar <esthermmakwe@gmail.com>
2022-07-02 09:01:36 -0700

    check

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of code running 
+ * prints alphabet in lowercase
+ * Return: 0 is successful
+ */
+int main(void)
+{
+	char a2z;
+
+	for (a2z = 'a'; a2z <= 'z'; a2z++
+	{)
+		putchar(a2z);
+	}
+	putchar('\n');
+	return (0);
+}
