36f1942aea1efa649c72345dec8249d6a7348d26
Kvngxthar <esthermmakwe@gmail.com>
2022-07-05 13:26:35 -0700

    God abeg

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 int _putchar(char c);
-ilphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10 (void);
 int _islower (int c);
 int _isalpha(int c);
-int print_ sign (int n);
+int print_ sign(int n);
 int _abs (int);
 int print_last_digit (int);
 void jack_bauer(void);
