24e7078f39470f0a7d0726a2e131893e8d05d1ae
Kvngxthar <esthermmakwe@gmail.com>
2022-07-05 21:38:42 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,41 +1,39 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_times_table - Prints the times table of the input,
- * starting with 0.
- * @n: The value of the times table to be printed.
+ * print_times_table - prints time table of n
+ * @n: takes number input
  */
 
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int prod, mult, num;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; num++)
+		for (num = 0; num <= n; ++num)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
 				_putchar(' ');
 
 				prod = num * mult;
 
-				if (prod <= 99)
-					_putchar(' ');
 				if (prod <= 9)
 					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
 				if (prod >= 100)
 				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
