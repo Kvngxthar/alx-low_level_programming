6527b13cbd384a6206e27d4c6f94395086623881
Kvngxthar <esthermmakwe@gmail.com>
2022-07-14 18:29:18 -0700

    trailing whitespaces

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,7 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-  /**
+  /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
@@ -18,7 +18,7 @@ int main(void)
    */
 
   *(p + 5) = 98;
-  / ...so that this prints 98\n/
+  /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
