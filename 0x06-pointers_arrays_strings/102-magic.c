aa00aa8ad733fa0956b3ac1bbbbe3c49aa7ee7a7
Kvngxthar <esthermmakwe@gmail.com>
2022-07-14 18:07:46 -0700

    commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,7 +14,6 @@ int main(void)
 	   * - you are not allowed to use a
 	   * - you are not allowed to modify p
 	   * - only one statement
-	   * main - prints a-[2] = 98
 	   * - you are not allowed to code anything else than this line of code
 	   */
 
