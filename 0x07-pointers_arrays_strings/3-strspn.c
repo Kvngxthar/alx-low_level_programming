561ab5a8c8368771dffadacaba89985aba79420b
Kvngxthar <esthermmakwe@gmail.com>
2022-07-18 04:42:13 -0700

    trailing whitespaces

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - Gets the length of a prefix substring.
  * @s: The string to be searched.
  * @accept: The prefix to be measured.
- * Return: The number of bytes 
+ * Return: The number of bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
