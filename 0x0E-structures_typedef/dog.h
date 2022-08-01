a57be1767796215d425fc027a7d5cf26234d89bb
Kvngxthar <esthermmakwe@gmail.com>
2022-08-01 15:25:57 -0700

    dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,6 +11,7 @@ typedef struct dog dog_t;
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * 
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
