8ab976384ca3044deeff67369b30a4d0a1a5ac82
Kvngxthar <esthermmakwe@gmail.com>
2022-08-22 21:49:52 -0700

    betty errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  *    * @text_content: content writed in the file.
  *     *
  *      * Return: 1 if it success. -1 if it fails.
- *       */
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
